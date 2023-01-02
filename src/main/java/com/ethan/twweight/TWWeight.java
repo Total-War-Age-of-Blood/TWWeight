05a0fbda5cf8e47c20cabae5f62bbd862199b047
Ethansito <ethansito@sbcglobal.net>
2023-01-01 23:10:14 -0600

    Fixed bug where speed gets reset on dimension change.

--- src/main/java/com/ethan/twweight/TWWeight.java
+++ src/main/java/com/ethan/twweight/TWWeight.java
@@ -4,7 +4,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPortalExitEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
@@ -32,6 +34,28 @@ public final class TWWeight extends JavaPlugin implements Listener {
             player.setVelocity(player.getLocation().getDirection().multiply(Math.max((0.2f - (0.025f * box_count)), 0.0f)));
             System.out.println("Swimming speed: " + player.getVelocity());
             System.out.println("Multiplier: " + (0.2f - (0.025f * box_count)));
+        }
+    }
+
+    @EventHandler
+    public void onChangeDimension(EntityPortalExitEvent event){
+        if (!(event.getEntity() instanceof Player)){return;}
+        Player player = (Player) event.getEntity();
+
+        ItemStack[] contents = player.getInventory().getContents();
+        float box_count = 0;
+        for (ItemStack item : contents){
+            if (item == null){continue;}
+            if(WeightCheck.SHULKER_NAMES.contains(item.getType())){
+                continue;}
+            box_count++;
+        }
+
+        if (box_count > 0){
+            float new_speed = player.getWalkSpeed()-(0.025f * box_count);
+            player.setWalkSpeed(Math.max(new_speed, 0));
+            player.setFlySpeed(Math.max(new_speed, 0));
+            WeightCheck.shulker_list.put(player.getUniqueId(), box_count);
         }
     }
 
--- src/main/java/com/ethan/twweight/WeightCheck.java
+++ src/main/java/com/ethan/twweight/WeightCheck.java
@@ -10,7 +10,7 @@ import java.util.*;
 
 public class WeightCheck extends BukkitRunnable {
     public static HashMap<UUID, Float> shulker_list = new HashMap<>();
-    List<Material> SHULKER_NAMES = new ArrayList<>(Arrays.asList(Material.SHULKER_BOX, Material.BLACK_SHULKER_BOX, Material.BLUE_SHULKER_BOX,
+    public static List<Material> SHULKER_NAMES = new ArrayList<>(Arrays.asList(Material.SHULKER_BOX, Material.BLACK_SHULKER_BOX, Material.BLUE_SHULKER_BOX,
     Material.BROWN_SHULKER_BOX, Material.CYAN_SHULKER_BOX, Material.GRAY_SHULKER_BOX, Material.LIGHT_BLUE_SHULKER_BOX, Material.LIGHT_GRAY_SHULKER_BOX,
     Material.LIME_SHULKER_BOX, Material.LIME_SHULKER_BOX, Material.MAGENTA_SHULKER_BOX, Material.ORANGE_SHULKER_BOX, Material.PINK_SHULKER_BOX,
     Material.PURPLE_SHULKER_BOX, Material.RED_SHULKER_BOX, Material.WHITE_SHULKER_BOX, Material.YELLOW_SHULKER_BOX));
